d302467c35ad5bd5b863a02b40ffed5e93905b39
Joe Blau <im@joeblau.com>
2022-10-01 23:13:41 -0700

    Add stats

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,6 +26,36 @@ const Home: NextPage = () => {
     overrides: { from: address },
   });
 
+  const { data: globalRankData } = useContractRead({
+    addressOrName: "0xca41f293A32d25c2216bC4B30f5b0Ab61b6ed2CB",
+    contractInterface: XenCrypto.abi,
+    functionName: "globalRank",
+    overrides: { from: address },
+  });
+
+  const { data: activeMintersData } = useContractRead({
+    addressOrName: "0xca41f293A32d25c2216bC4B30f5b0Ab61b6ed2CB",
+    contractInterface: XenCrypto.abi,
+    functionName: "activeMinters",
+    overrides: { from: address },
+  });
+
+  const { data: activeStakesData } = useContractRead({
+    addressOrName: "0xca41f293A32d25c2216bC4B30f5b0Ab61b6ed2CB",
+    contractInterface: XenCrypto.abi,
+    functionName: "activeStakes",
+    overrides: { from: address },
+  });
+
+  const { data: totalXenStakedData } = useContractRead({
+    addressOrName: "0xca41f293A32d25c2216bC4B30f5b0Ab61b6ed2CB",
+    contractInterface: XenCrypto.abi,
+    functionName: "totalXenStaked",
+    overrides: { from: address },
+  });
+
+  console.log(activeMintersData);
+
   const shortenAddress = (address: string) => {
     return address.slice(0, 6) + "..." + address.slice(-4);
   };
@@ -48,20 +78,42 @@ const Home: NextPage = () => {
         ) : (
           <button onClick={() => connect()}>Connect Wallet</button>
         )}
+        <h2>Stats</h2>
+        <ul>
+          <li>Global Rank: {globalRankData && globalRankData.toString()}</li>
+          <li>
+            Active Minters: {activeMintersData && activeMintersData.toString()}
+          </li>
+          <li>
+            Active Stakes: {activeStakesData && activeStakesData.toString()}
+          </li>
+          <li>
+            Total XEN Staked:{" "}
+            {totalXenStakedData && Number(totalXenStakedData) / 1e18}
+          </li>
+        </ul>
         <h2>Mint</h2>
         <ul>
-          <li>amplifier {userMintData?.amplifier.toString()}</li>
-          <li>eaaRate {userMintData?.eaaRate.toString()}</li>
-          <li>maturityTs {userMintData?.maturityTs.toString()}</li>
-          <li>rank {userMintData?.rank.toString()}</li>
-          <li>term {userMintData?.term.toString()}</li>
+          {userMintData && (
+            <>
+              <li>amplifier {userMintData.amplifier.toString()}</li>
+              <li>eaaRate {userMintData.eaaRate.toString()}</li>
+              <li>maturityTs {userMintData.maturityTs.toString()}</li>
+              <li>rank {userMintData.rank.toString()}</li>
+              <li>term {userMintData.term.toString()}</li>
+            </>
+          )}
         </ul>
         <h2>Stake</h2>
         <ul>
-          <li>amount {userStakeData?.amount.toString()}</li>
-          <li>apy {userStakeData?.apy.toString()}</li>
-          <li>maturityTs {userStakeData?.maturityTs.toString()}</li>
-          <li>term {userStakeData?.term.toString()}</li>
+          {userStakeData && (
+            <>
+              <li>amount {userStakeData.amount.toString()}</li>
+              <li>apy {userStakeData.apy.toString()}</li>
+              <li>maturityTs {userStakeData.maturityTs.toString()}</li>
+              <li>term {userStakeData.term.toString()}</li>
+            </>
+          )}
         </ul>
       </main>
     </div>
