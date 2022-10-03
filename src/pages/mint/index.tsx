68a5b714048cca8860f14329df36002a125de45c
Joe Blau <im@joeblau.com>
2022-10-03 10:05:42 -0700

    Update stake progress bar

--- src/pages/mint/index.tsx
+++ src/pages/mint/index.tsx
@@ -3,9 +3,28 @@ import Container from "~/components/Container";
 import { useContractRead } from "wagmi";
 import XenCrypto from "~/abi/XENCrypto.json";
 import CountUp from "react-countup";
+import { useStep } from "usehooks-ts";
+import { clsx } from "clsx";
+
+const steps: any[] = [
+  {
+    id: 1,
+    title: "Start Mint",
+  },
+  {
+    id: 2,
+    title: "Minting",
+  },
+  {
+    id: 3,
+    title: "Claim",
+  },
+];
 
 const Mint = () => {
   const { address } = useAccount();
+  const [currentStep, helpers] = useStep(steps.length);
+  const { goToNextStep, setStep } = helpers;
 
   const { data: userMintData } = useContractRead({
     addressOrName: "0xca41f293A32d25c2216bC4B30f5b0Ab61b6ed2CB",
@@ -44,10 +63,6 @@ const Mint = () => {
     return userMintData?.term - daysRemaining();
   };
 
-  const estimatedXEN = () => {
-    return 30;
-  };
-
   const MintProgress = () => {
     return (
       <div className="stat">
@@ -81,64 +96,193 @@ const Mint = () => {
     );
   };
 
+  const PercnetageField = () => {
+    return (
+      <div className="form-control w-full">
+        <label className="label text-neutral">
+          <span className="label-text text-neutral">SHARE PERCENTAGE</span>
+          <span className="label-text-alt text-error">Required</span>
+        </label>
+        <input
+          type="text"
+          placeholder="1% — 100%"
+          className="input input-bordered w-full text-neutral"
+        />
+        <label className="label">
+          <span className="label-text-alt text-neutral">
+            The percentage of XEN between 1% and 100%
+          </span>
+        </label>
+      </div>
+    );
+  };
+
+  const DaysField = () => {
+    return (
+      <div className="form-control w-full">
+        <label className="label text-neutral">
+          <span className="label-text text-neutral">MINT DAYS</span>
+          <span className="label-text-alt text-error">Required</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Number of days"
+          className="input input-bordered w-full text-neutral"
+        />
+        <label className="label">
+          <span className="label-text-alt text-neutral">
+            Select the number of days you want to mint between 0 and 100
+          </span>
+        </label>
+      </div>
+    );
+  };
+
+  const Claim = () => {
+    return (
+      <div className="flex flex-col space-y-4">
+        <h2 className="card-title text-neutral">Claim</h2>
+        <DaysField />
+        <button className="btn glass text-neutral">Start Mint</button>
+      </div>
+    );
+  };
+
+  const ClaimShare = () => {
+    return (
+      <div className="flex flex-col space-y-4">
+        <h2 className="card-title text-neutral">Claim + Share</h2>
+        <div className="form-control w-full">
+          <label className="label text-neutral">
+            <span className="label-text text-neutral">WALLET ADDRESS</span>
+            <span className="label-text-alt text-error">Required</span>
+          </label>
+          <input
+            type="text"
+            placeholder="0x...."
+            className="input input-bordered w-full text-neutral"
+          />
+          <label className="label">
+            <span className="label-text-alt text-neutral">
+              Wallet address where you want to share your XEN
+            </span>
+          </label>
+        </div>
+
+        <PercnetageField />
+
+        <button className="btn glass text-neutral">Claim + Share</button>
+      </div>
+    );
+  };
+
+  const ClaimStake = () => {
+    return (
+      <div className="flex flex-col space-y-4">
+        <h2 className="card-title text-neutral">Claim + Stake</h2>
+
+        <PercnetageField />
+        <DaysField />
+
+        <button className="btn glass text-neutral">Claim + Stake</button>
+      </div>
+    );
+  };
+
+  const StartMint = () => {
+    return (
+      <div className="flex flex-col space-y-4">
+        <h2 className="card-title text-neutral">Start Mint</h2>
+        <div className="form-control w-full">
+          <label className="label text-neutral">
+            <span className="label-text text-neutral">MINT DAYS</span>
+            <span className="label-text-alt text-error">Required</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Type days"
+            className="input input-bordered w-full text-neutral"
+          />
+          <label className="label">
+            <span className="label-text-alt text-neutral">
+              Select the number of days you want to mint between 0 and 100
+            </span>
+          </label>
+        </div>
+        <button className="btn glass text-neutral">Start Mint</button>
+      </div>
+    );
+  };
+
+  const MintingStep = () => {
+    return (
+      <>
+        <h2 className="card-title">Minting</h2>
+        <div className="stats stats-vertical bg-transparent text-neutral">
+          <MintProgress />
+          {mintItems.map((item, index) => (
+            <div className="stat" key={index}>
+              <div className="stat-title">{item.title}</div>
+              <code className="stat-value text-lg md:text-3xl text-right">
+                <CountUp
+                  end={item.value}
+                  preserveValue={true}
+                  separator=","
+                  suffix={item.suffix}
+                  // decimals={2}
+                />
+              </code>
+              <div className="stat-desc text-right"></div>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  };
+
+  const ClaimStep = () => {
+    return (
+      <div className="flex flex-col w-full border-opacity-50">
+        <Claim />
+        <div className="divider">OR</div>
+        <ClaimShare />
+        <div className="divider">OR</div>
+        <ClaimStake />
+      </div>
+    );
+  };
+
   return (
     <Container>
       <div className="flew flex-row space-y-8 ">
         <ul className="steps w-full">
-          <li className="step step-neutral">Start Mint</li>
-          <li className="step step-neutral">Minting</li>
-          <li className="step">Claim</li>
+          {steps.map((step, index) => {
+            return (
+              <button
+                onClick={() => setStep(step.id)}
+                key={index}
+                className={clsx("step", {
+                  "step-neutral": currentStep >= step.id,
+                })}
+              >
+                {step.title}
+              </button>
+            );
+          })}
         </ul>
 
         <div className="card glass">
           <div className="card-body">
-            {userMintData?.user.toString() ===
-            "0x0000000000000000000000000000000000000000" ? (
-              <div className="flex flex-col space-y-4">
-                <h2 className="card-title text-neutral">Mint XEN</h2>
-                <div className="form-control w-full">
-                  <label className="label text-neutral">
-                    <span className="label-text text-neutral">MINT DAYS</span>
-                    <span className="label-text-alt text-error">Required</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="Type days"
-                    className="input input-bordered w-full text-neutral"
-                  />
-                  <label className="label">
-                    <span className="label-text-alt text-neutral">
-                      Select the number of days you want to mint between 0 and
-                      100
-                    </span>
-                  </label>
-                </div>
-                <button className="btn glass text-neutral">Start Mint</button>
-              </div>
-            ) : (
-              <>
-                <h2 className="card-title">Mint</h2>
-
-                <div className="stats stats-vertical bg-transparent text-neutral">
-                  <MintProgress />
-                  {mintItems.map((item, index) => (
-                    <div className="stat" key={index}>
-                      <div className="stat-title">{item.title}</div>
-                      <code className="stat-value text-lg md:text-3xl text-right">
-                        <CountUp
-                          end={item.value}
-                          preserveValue={true}
-                          separator=","
-                          suffix={item.suffix}
-                          // decimals={2}
-                        />
-                      </code>
-                      <div className="stat-desc text-right"></div>
-                    </div>
-                  ))}
-                </div>
-              </>
-            )}
+            {(() => {
+              switch (currentStep) {
+                case 2:
+                  return <MintingStep />;
+                case 3:
+                  return <ClaimStep />;
+                default:
+                  return <StartMint />;
+              }
+            })()}
           </div>
         </div>
       </div>
--- src/pages/stake/index.tsx
+++ src/pages/stake/index.tsx
@@ -2,9 +2,29 @@ import { useAccount } from "wagmi";
 import Container from "~/components/Container";
 import { useContractRead, useBalance } from "wagmi";
 import XenCrypto from "~/abi/XENCrypto.json";
+import { useStep } from "usehooks-ts";
+import { clsx } from "clsx";
+
+const steps: any[] = [
+  {
+    id: 1,
+    title: "Start Stake",
+  },
+  {
+    id: 2,
+    title: "Staking",
+  },
+  {
+    id: 3,
+    title: "End Stake",
+  },
+];
 
 const Stake = () => {
   const { address } = useAccount();
+  const [currentStep, helpers] = useStep(steps.length);
+  const { goToNextStep, setStep } = helpers;
+
   const { data: balanceData } = useBalance({
     addressOrName: address,
     token: "0xca41f293A32d25c2216bC4B30f5b0Ab61b6ed2CB",
@@ -16,20 +36,28 @@ const Stake = () => {
     functionName: "getUserStake",
     overrides: { from: address },
   });
-  console.log(balanceData);
+
   return (
     <Container>
       <div className="flew flex-row space-y-8 ">
         <ul className="steps w-full">
-          <li className="step step-neutral">Start Stake</li>
-          <li className="step step-neutral">Staking</li>
-          <li className="step">End Stake</li>
+          {steps.map((step, index) => {
+            return (
+              <button
+                onClick={() => setStep(step.id)}
+                key={index}
+                className={clsx("step", {
+                  "step-neutral": currentStep >= step.id,
+                })}
+              >
+                {step.title}
+              </button>
+            );
+          })}
         </ul>
         <div className="card glass">
           <div className="card-body">
-            <h2 className="card-title">Stake</h2>
-            {/* {balance} */}
-            <ul>
+            {/* <ul>
               {userStakeData && (
                 <>
                   <li>balance {balanceData?.formatted}</li>
@@ -39,7 +67,7 @@ const Stake = () => {
                   <li>term {userStakeData.term.toString()}</li>
                 </>
               )}
-            </ul>
+            </ul> */}
           </div>
         </div>
       </div>
