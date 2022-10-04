ad9e462cd4bc904235356e9b9c28d0199933f8a7
Joe Blau <im@joeblau.com>
2022-10-04 11:22:59 -0700

    add infura provider

---
+++ src/lib/client.ts
@@ -0,0 +1,65 @@
+import { createClient, configureChains, chain } from "wagmi";
+import { pulseChain } from "../lib/pulsechain";
+
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { infuraProvider } from "wagmi/providers/infura";
+import { publicProvider } from "wagmi/providers/public";
+
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { InjectedConnector } from "wagmi/connectors/injected";
+
+const alchemyId = process.env.ALCHEMY_ID;
+const infuraId = process.env.INFURA_ID;
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [chain.mainnet, chain.goerli, pulseChain],
+  [
+    alchemyProvider({ apiKey: alchemyId, priority: 0 }),
+    infuraProvider({ apiKey: infuraId, priority: 0 }),
+    jsonRpcProvider({
+      priority: 0,
+      rpc: (chain) => {
+        if (chain.id !== pulseChain.id) return null;
+        return { http: chain.rpcUrls.default };
+      },
+    }),
+    publicProvider({ priority: 1 }),
+  ]
+);
+
+export const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({
+      chains,
+      options: {
+        shimDisconnect: false,
+        shimChainChangedDisconnect: false,
+      },
+    }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: "xen.fyi",
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        qrcode: true,
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: "Injected",
+        shimDisconnect: false,
+      },
+    }),
+  ],
+  provider,
+  webSocketProvider,
+});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,68 +1,11 @@
 import "~/styles/main.css";
-import { WagmiConfig, createClient, configureChains, chain } from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
 import type { AppProps } from "next/app";
-import { pulseChain } from "../lib/pulsechain";
-import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { WagmiConfig } from "wagmi";
 import { ConnectKitProvider } from "connectkit";
-import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
-import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
-import { InjectedConnector } from "wagmi/connectors/injected";
-import Layout from "~/components/Layout";
 import { ThemeProvider } from "next-themes";
-import { publicProvider } from "wagmi/providers/public";
 import { GoogleAnalytics } from "nextjs-google-analytics";
-
-const alchemyId = process.env.ALCHEMY_ID;
-
-const { chains, provider, webSocketProvider } = configureChains(
-  [chain.mainnet, chain.goerli, pulseChain],
-  [
-    alchemyProvider({ apiKey: alchemyId }),
-    jsonRpcProvider({
-      rpc: (chain) => {
-        if (chain.id !== pulseChain.id) return null;
-        return { http: chain.rpcUrls.default };
-      },
-    }),
-    publicProvider(),
-  ]
-);
-
-const client = createClient({
-  autoConnect: true,
-  connectors: [
-    new MetaMaskConnector({
-      chains,
-      options: {
-        shimDisconnect: false,
-        shimChainChangedDisconnect: false,
-      },
-    }),
-    new CoinbaseWalletConnector({
-      chains,
-      options: {
-        appName: "xen.fyi",
-      },
-    }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        qrcode: true,
-      },
-    }),
-    new InjectedConnector({
-      chains,
-      options: {
-        name: "Injected",
-        shimDisconnect: false,
-      },
-    }),
-  ],
-  provider,
-  webSocketProvider,
-});
+import { client } from "~/lib/client";
+import Layout from "~/components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
