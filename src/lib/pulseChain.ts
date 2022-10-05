9fcfb07675b333103fb7593b10d24cb34636031b
Joe Blau <im@joeblau.com>
2022-10-04 23:31:17 -0700

    remove pulse chain multicall

--- src/lib/pulseChain.ts
+++ src/lib/pulseChain.ts
@@ -18,9 +18,5 @@ export const pulseChain: Chain = {
       url: "https://scan.v2b.testnet.pulsechain.com",
     },
   },
-  multicall: {
-    address: "0x5ba1e12693dc8f9c48aad8770482f4739beed696",
-    blockCreated: 12336033,
-  },
   testnet: true,
 };
