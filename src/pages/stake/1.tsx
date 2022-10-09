38e5f9b8b9aa83d350dd4ad1f30a68bea7521573
Joe Blau <im@joeblau.com>
2022-10-09 11:03:40 -0700

    fix max start stake

--- src/pages/stake/1.tsx
+++ src/pages/stake/1.tsx
@@ -39,12 +39,14 @@ const Stake = () => {
   /*** CONTRACT READ SETUP  ***/
 
   const { data: balanceData } = useBalance({
-    ...xenContract(chain),
+    addressOrName: address,
+    token: xenContract(chain).addressOrName,
     watch: true,
   });
 
   const { data: userStake } = useContractRead({
-    ...xenContract(chain),
+    addressOrName: "0xca41f293A32d25c2216bC4B30f5b0Ab61b6ed2CB",
+    contractInterface: XenCrypto.abi,
     functionName: "getUserStake",
     overrides: { from: address },
     watch: true,
