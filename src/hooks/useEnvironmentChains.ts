0aed05ca5897cc6e74d6b07256d0be41f3a8c4da
Joe Blau <im@joeblau.com>
2022-11-16 14:15:33 -0800

    add router preview state

--- src/hooks/useEnvironmentChains.ts
+++ src/hooks/useEnvironmentChains.ts
@@ -1,12 +1,14 @@
 import { Chain, useSwitchNetwork } from "wagmi";
 import { chainList } from "~/lib/client";
+import { useRouter } from "next/router";
 
 export const useEnvironmentChains = () => {
+  const router = useRouter();
   const env = process.env.NODE_ENV as string;
 
   return {
     envChains: chainList.filter((chain: Chain) =>
-      env == "production" ? !chain.testnet : chain.testnet
+      env == "production" && !router.isPreview ? !chain.testnet : chain.testnet
     ),
   };
 };
